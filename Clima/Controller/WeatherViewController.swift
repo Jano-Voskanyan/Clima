24ec671c900f004eb3f3a476eb5af1744a04e605
Janibek <jano.vr@mail.ru>
2023-02-14 13:53:52 +0400

    change interface style (Dark/Light mode)

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
