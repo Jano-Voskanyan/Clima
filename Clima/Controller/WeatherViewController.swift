15a0102907d28281fa17c955b9e38a44a5f9cb68
Janibek <jano.vr@mail.ru>
2023-02-15 21:07:49 +0400

    add UITextField, add UITextFieldDelegate write functions` 1. textFieldShouldReturn 2. textFieldShouldEndEditing 3. textFieldDidEndEditing

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController,
+                             UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     
     override func viewDidLoad() {
@@ -19,8 +21,35 @@ class WeatherViewController: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tapGesture)
+        
+        searchTextField.delegate = self
+        
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            textField.placeholder = "Search"
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField to get the weather for that city.
+        
+        searchTextField.text = ""
+    }
 }
 
